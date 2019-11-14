6284dd34d9103a0247f379508d656d9d0e34a183
John Tokhi <56596400+tokhij@users.noreply.github.com>
2019-11-14 13:25:27 -0500

    completed ROT13 part

--- Crypto/src/ROT13.java
+++ Crypto/src/ROT13.java
@@ -1,32 +1,61 @@
+package src;
+
 import static java.lang.Character.isLowerCase;
 import static java.lang.Character.isUpperCase;
 import static java.lang.Character.toLowerCase;
 
 public class ROT13  {
 
+    private String lowercaseStart = "abcdefghijklmnopqrstuvwxyz";
+    private String uppercaseStart = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private String startUpper;
+    private String registerUpper;
+    private String startLower;
+    private String registerLower;
+    private boolean symmetric = false;
+
     ROT13(Character cs, Character cf) {
+        if(toLowerCase(cs) == 'a' &&
+        toLowerCase(cf) == 'n') symmetric = true;
+
+        startUpper = ROT13.rotate(uppercaseStart, Character.toUpperCase(cs));
+        startLower = ROT13.rotate(lowercaseStart, Character.toLowerCase(cs));
+        registerUpper = ROT13.rotate(uppercaseStart, Character.toUpperCase(cf));
+        registerLower = ROT13.rotate(lowercaseStart, Character.toLowerCase(cf));
+
+        System.out.println(startUpper);
+        System.out.println(registerUpper);
+        System.out.println(startLower);
+        System.out.println(registerLower);
     }
 
     ROT13() {
+        this( 'a',  'n');
     }
 
 
     public String crypt(String text) throws UnsupportedOperationException {
-
-        return "";
+        if (this.symmetric != true) throw new UnsupportedOperationException();
+        return substituteIn(text, startUpper, registerUpper, startLower, registerLower);
     }
 
     public String encrypt(String text) {
-        return text;
+        return substituteIn(text, startUpper, registerUpper, startLower, registerLower);
     }
 
     public String decrypt(String text) {
-        return text;
+        return substituteIn(text, startUpper, registerUpper, startLower, registerLower);
     }
 
     public static String rotate(String s, Character c) {
+        int offset = s.indexOf(c);
+        int i = offset % s.length();
 
-        return "";
+        return s.substring(i) + s.substring(0,1);
+    }
+
+    private String substituteIn(String text, String startUpper, String registerUpper, String startLower, String registerLower){
+        return substituteIn(text, startUpper, registerUpper, startLower, registerLower);
     }
 
 }
